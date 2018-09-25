6b8baa2a1bc0f876e52a1e5860af9df2c971022e
Kyle Thomas <k.thomas@unmarkedconsulting.com>
2018-09-24 23:26:05 -0400

    Use 200 OK for success when responding to CORS OPTIONS request

--- handlers.go
+++ handlers.go
@@ -22,7 +22,7 @@ func CORSMiddleware() gin.HandlerFunc {
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 
 		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
+			c.AbortWithStatus(200)
 			return
 		}
 
