2f602bbdc64019552884a828ae2bf5555d8817dd
Kyle Thomas <kyle@provide.services>
2019-09-30 15:29:37 -0400

    Add auto incrementing model base

--- models.go
+++ models.go
@@ -5,8 +5,15 @@ import (
 	"time"
 
 	"github.com/jinzhu/gorm"
-	"github.com/kthomas/go.uuid"
+	uuid "github.com/kthomas/go.uuid"
 )
+
+// AutoIncrementingModel base class with int primary key
+type AutoIncrementingModel struct {
+	ID        uint      `gorm:"primary_key;column:id;default:nextval('accounts_id_seq'::regclass)" json:"id"`
+	CreatedAt time.Time `sql:"not null;default:now()" json:"created_at"`
+	Errors    []*Error  `sql:"-" json:"-"`
+}
 
 // Model base class with uuid v4 primary key id
 type Model struct {
