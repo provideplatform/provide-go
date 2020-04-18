4719a91fe86a8cb04673ea94efad04240c5b9b87
Kyle Thomas <kyle@provide.services>
2020-04-18 14:53:32 -0400

    Add ecdh shared secret wrapper

--- ecdh.go
+++ ecdh.go
@@ -1,11 +1,29 @@
 package provide
 
 import (
+	"crypto"
 	"crypto/rand"
 	"fmt"
+	"math/big"
 
 	"github.com/aead/ecdh"
 )
+
+type point struct {
+	X, Y *big.Int
+}
+
+// C25519ComputeSecret - computes the shared secret given a private key and peer's public key
+func C25519ComputeSecret(privateKey, peerPublicKey []byte) []byte {
+	var privkey crypto.PrivateKey
+	privkey = privateKey
+
+	var pubkey crypto.PublicKey
+	pubkey = peerPublicKey
+
+	c25519 := ecdh.X25519()
+	return c25519.ComputeSecret(privkey, pubkey)
+}
 
 // C25519GenerateKeyPair - generates a c25519 keypair suitable for Diffie-Hellman key exchange
 func C25519GenerateKeyPair() (publicKey, privateKey []byte, err error) {
@@ -26,4 +44,30 @@ func C25519GenerateKeyPair() (publicKey, privateKey []byte, err error) {
 
 	log.Debugf("generated c25519 keypair with public key: %s", string(publicKey))
 	return publicKey, privateKey, nil
+}
+
+func checkPrivateKey(typ interface{}) (key []byte, ok bool) {
+	switch t := typ.(type) {
+	case []byte:
+		key = t
+		ok = true
+	case *[]byte:
+		key = *t
+		ok = true
+	}
+
+	return key, ok
+}
+
+func checkPublicKey(typ interface{}) (key point, ok bool) {
+	switch t := typ.(type) {
+	case point:
+		key = t
+		ok = true
+	case *point:
+		key = *t
+		ok = true
+	}
+
+	return key, ok
 }
