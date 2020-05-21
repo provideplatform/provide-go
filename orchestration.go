dacf41655f7ce63ec0a9f45ba8b18759ee8cc4bd
Marat <marat.kamenschikov@gmail.com>
2020-05-22 01:14:49 +0300

    making ContainerParams fields public

--- orchestration.go
+++ orchestration.go
@@ -2,17 +2,17 @@ package provide
 
 // ContainerParams is a structure of params common to AWS and Azure containers
 type ContainerParams struct {
-	region            string
-	resourceGroupName string
-	image             *string
-	virtualNetworkID  *string
-	cpu               *int64
-	memory            *int64
-	entrypoint        []*string
-	securityGroupIds  []string
-	subnetIds         []string
-	environment       map[string]interface{}
-	security          map[string]interface{}
+	Region            string
+	ResourceGroupName string
+	Image             *string
+	VirtualNetworkID  *string
+	Cpu               *int64
+	Memory            *int64
+	Entrypoint        []*string
+	SecurityGroupIds  []string
+	SubnetIds         []string
+	Environment       map[string]interface{}
+	Security          map[string]interface{}
 }
 
 // TargetCredentials struct has all credentials to access AWS and Azure in one place
