0da2792e14e2bad71ad2216a823c039598c569da
eoinco <eoin.connolly@gmail.com>
2020-07-22 18:37:45 +0100

    changes for rsa sign algorithm parameter

--- vault.go
+++ vault.go
@@ -70,19 +70,21 @@ func DeleteVaultKey(token, vaultID, keyID string) (int, interface{}, error) {
 }
 
 // SignMessage signs a message with the given key
-func SignMessage(token, vaultID, keyID, msg string) (int, interface{}, error) {
+func SignMessage(token, vaultID, keyID, msg, algo string) (int, interface{}, error) {
 	uri := fmt.Sprintf("vaults/%s/keys/%s/sign", vaultID, keyID)
 	return InitVault(stringOrNil(token)).Post(uri, map[string]interface{}{
-		"message": msg,
+		"message":   msg,
+		"algorithm": algo,
 	})
 }
 
 // VerifySignature verifies a signature
-func VerifySignature(token, vaultID, keyID, msg, sig string) (int, interface{}, error) {
+func VerifySignature(token, vaultID, keyID, msg, sig, algo string) (int, interface{}, error) {
 	uri := fmt.Sprintf("vaults/%s/keys/%s/verify", vaultID, keyID)
 	return InitVault(stringOrNil(token)).Post(uri, map[string]interface{}{
 		"message":   msg,
 		"signature": sig,
+		"algorithm": algo,
 	})
 }
 
