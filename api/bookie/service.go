b932fe680f1ecc6c27027bc433b3a302b4bf40b2
Kyle Thomas <kyle@provide.services>
2020-09-15 12:34:05 -0400

    Initialize bookie api client

---
+++ api/bookie/service.go
@@ -0,0 +1,71 @@
+package ident
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/provideservices/provide-go/api"
+	"github.com/provideservices/provide-go/common"
+)
+
+const defaultBookieHost = "bookie.provide.services"
+const defaultBookiePath = "api/v1"
+const defaultBookieScheme = "https"
+
+// Service for the ident api
+type Service struct {
+	api.Client
+}
+
+// InitBookieService convenience method to initialize a `bookie.Service` instance
+func InitBookieService(token *string) *Service {
+	host := defaultBookieHost
+	if os.Getenv("BOOKIE_API_HOST") != "" {
+		host = os.Getenv("BOOKIE_API_HOST")
+	}
+
+	path := defaultBookiePath
+	if os.Getenv("BOOKIE_API_PATH") != "" {
+		host = os.Getenv("BOOKIE_API_PATH")
+	}
+
+	scheme := defaultBookiePath
+	if os.Getenv("BOOKIE_API_SCHEME") != "" {
+		scheme = os.Getenv("BOOKIE_API_SCHEME")
+	}
+
+	return &Service{
+		api.Client{
+			Host:   host,
+			Path:   path,
+			Scheme: scheme,
+			Token:  token,
+		},
+	}
+}
+
+// BroadcastPayment attempts to broadcast a payment using the given params
+// FIXME-- this is a proof of concept for now...
+func BroadcastPayment(token, applicationID string, params map[string]interface{}) (map[string]interface{}, error) {
+	uri := fmt.Sprintf("payments/%s", applicationID)
+	status, resp, err := InitBookieService(common.StringOrNil(token)).Put(uri, params)
+	if err != nil {
+		return nil, err
+	}
+
+	if status != 201 {
+		return nil, fmt.Errorf("failed to create payment; status: %v", status)
+	}
+
+	// FIXME...
+	response := map[string]interface{}{}
+	raw, _ := json.Marshal(resp)
+	err = json.Unmarshal(raw, &response)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to create payment; status: %v; %s", status, err.Error())
+	}
+
+	return response, nil
+}
