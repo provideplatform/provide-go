e5ea31f36dc20a8a027d7ec31eb85765049a99d2
Kyle Thomas <kyle@provide.services>
2020-09-22 02:46:29 -0400

    Revert "Use tx.hash"
    This reverts commit cbe723a281cdfa4f27835087e370f67f368d8119.

--- crypto/ethereum.go
+++ crypto/ethereum.go
@@ -495,7 +495,7 @@ func EVMTxFactory(
 	}
 
 	signer := types.MakeSigner(cfg, big.NewInt(int64(blockNumber)))
-	hash := tx.Hash().Bytes()
+	hash := signer.Hash(tx).Bytes()
 
 	return signer, tx, hash, err
 }
