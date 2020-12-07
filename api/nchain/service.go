cbbcf3a37eb9fa2fd32454e37c91344da0b3ddf6
eoinco <eoin.connolly@gmail.com>
2020-12-07 15:27:25 +0000

    fixing bug with nchain host api

--- api/nchain/service.go
+++ api/nchain/service.go
@@ -27,7 +27,7 @@ func InitNChainService(token string) *Service {
 
 	path := defaultNChainPath
 	if os.Getenv("NCHAIN_API_PATH") != "" {
-		host = os.Getenv("NCHAIN_API_PATH")
+		path = os.Getenv("NCHAIN_API_PATH")
 	}
 
 	scheme := defaultNChainScheme
