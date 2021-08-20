43b3d7914fedad2359f7346dbd9597fa55a947cc
Stefan Kostic <skostic9242@gmail.com>
2021-08-20 16:02:59 +0200

    Add tendermint tx and block types

--- api/nchain/models.go
+++ api/nchain/models.go
@@ -384,3 +384,94 @@ type BaseledgerBlockHeader struct {
 		Block string `json:"block"`
 	} `json:"version"`
 }
+
+type TendermintTx struct {
+	Jsonrpc string `json:"jsonrpc"`
+	ID      int    `json:"id"`
+	Result  struct {
+		Hash     string `json:"hash"`
+		Height   string `json:"height"`
+		Index    int    `json:"index"`
+		TxResult struct {
+			Code      int    `json:"code"`
+			Data      string `json:"data"`
+			Log       string `json:"log"`
+			Info      string `json:"info"`
+			GasWanted string `json:"gas_wanted"`
+			GasUsed   string `json:"gas_used"`
+			Events    []struct {
+				Type       string `json:"type"`
+				Attributes []struct {
+					Key   string `json:"key"`
+					Value string `json:"value"`
+					Index bool   `json:"index"`
+				} `json:"attributes"`
+			} `json:"events"`
+			Codespace string `json:"codespace"`
+		} `json:"tx_result"`
+		Tx string `json:"tx"`
+	} `json:"result"`
+}
+
+type TendermintBlock struct {
+	Jsonrpc string `json:"jsonrpc"`
+	ID      int    `json:"id"`
+	Result  struct {
+		BlockID struct {
+			Hash  string `json:"hash"`
+			Parts struct {
+				Total int    `json:"total"`
+				Hash  string `json:"hash"`
+			} `json:"parts"`
+		} `json:"block_id"`
+		Block struct {
+			Header struct {
+				Version struct {
+					Block string `json:"block"`
+				} `json:"version"`
+				ChainID     string    `json:"chain_id"`
+				Height      string    `json:"height"`
+				Time        time.Time `json:"time"`
+				LastBlockID struct {
+					Hash  string `json:"hash"`
+					Parts struct {
+						Total int    `json:"total"`
+						Hash  string `json:"hash"`
+					} `json:"parts"`
+				} `json:"last_block_id"`
+				LastCommitHash     string `json:"last_commit_hash"`
+				DataHash           string `json:"data_hash"`
+				ValidatorsHash     string `json:"validators_hash"`
+				NextValidatorsHash string `json:"next_validators_hash"`
+				ConsensusHash      string `json:"consensus_hash"`
+				AppHash            string `json:"app_hash"`
+				LastResultsHash    string `json:"last_results_hash"`
+				EvidenceHash       string `json:"evidence_hash"`
+				ProposerAddress    string `json:"proposer_address"`
+			} `json:"header"`
+			Data struct {
+				Txs []string `json:"txs"`
+			} `json:"data"`
+			Evidence struct {
+				Evidence []interface{} `json:"evidence"`
+			} `json:"evidence"`
+			LastCommit struct {
+				Height  string `json:"height"`
+				Round   int    `json:"round"`
+				BlockID struct {
+					Hash  string `json:"hash"`
+					Parts struct {
+						Total int    `json:"total"`
+						Hash  string `json:"hash"`
+					} `json:"parts"`
+				} `json:"block_id"`
+				Signatures []struct {
+					BlockIDFlag      int       `json:"block_id_flag"`
+					ValidatorAddress string    `json:"validator_address"`
+					Timestamp        time.Time `json:"timestamp"`
+					Signature        string    `json:"signature"`
+				} `json:"signatures"`
+			} `json:"last_commit"`
+		} `json:"block"`
+	} `json:"result"`
+}
