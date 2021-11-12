d8e2dc34dd0ff8e2f47378bef2664780c488c596
Kyle Thomas <kyle@provide.services>
2021-11-12 12:46:23 -0500

    Update baseline mapping fields

--- api/baseline/models.go
+++ api/baseline/models.go
@@ -56,24 +56,28 @@ type IssueVerifiableCredentialResponse struct {
 
 // Mapping for arbitrary model transformations
 type Mapping struct {
-	Models   []*MappingModel        `json:"models"`
-	Metadata map[string]interface{} `json:"metadata"`
-	Type     *string                `json:"type"`
+	Metadata    map[string]interface{} `json:"metadata"`
+	Models      []*MappingModel        `json:"models"`
+	Name        string                 `json:"name"`
+	Description *string                `json:"description"`
+	Type        *string                `json:"type"`
 }
 
 // MappingField for mapping
 type MappingField struct {
-	DefaultVault interface{} `json:"default_value,omitempty"`
+	DefaultValue interface{} `json:"default_value,omitempty"`
 	IsPrimaryKey bool        `json:"is_primary_key"`
 	Name         string      `json:"name"`
+	Description  *string     `json:"description"`
 	Type         string      `json:"type"`
 }
 
 // MappingModel consists of fields for mapping
 type MappingModel struct {
-	Fields     []*MappingField `json:"fields"`
-	PrimaryKey *string         `json:"primary_key"`
-	Type       *string         `json:"type"`
+	Description *string         `json:"description"`
+	Fields      []*MappingField `json:"fields"`
+	PrimaryKey  *string         `json:"primary_key"`
+	Type        *string         `json:"type"`
 }
 
 // Message is a proxy-internal wrapper for protocol message handling
