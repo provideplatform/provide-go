46885782be9eec67a8479ec3233784693d590979
Kyle Thomas <kyle@provide.services>
2021-11-27 00:07:07 -0500

    Add name to workstep

--- api/baseline/models.go
+++ api/baseline/models.go
@@ -161,6 +161,7 @@ type WorkflowInstance struct {
 // Workstep is a baseline workstep context
 type Workstep struct {
 	api.Model
+	Name            *string          `json:"prover,omitempty"`
 	Prover          *privacy.Circuit `json:"prover,omitempty"`
 	ProverID        *uuid.UUID       `json:"prover_id"`
 	Participants    []*Participant   `gorm:"many2many:worksteps_participants" json:"participants,omitempty"`
